206f121fce0a432967ea1056b985f29af17a1f6b
RisathMa <risath.manvidu50@gmail.com>
2026-02-28 18:42:35 +0530

    Fix: Bind Cowboy server to 0.0.0.0 and listen on PORT env var

--- apps/secure_chat/src/secure_chat_app.erl
+++ apps/secure_chat/src/secure_chat_app.erl
@@ -11,8 +11,10 @@ start(_StartType, _StartArgs) ->
             {"/ws", ws_handler, []}
         ]}
     ]),
+    PortStr = os:getenv("PORT", "8080"),
+    {Port, _} = string:to_integer(PortStr),
     {ok, _} = cowboy:start_clear(my_http_listener,
-        [{port, 8080}],
+        [{ip, {0,0,0,0}}, {port, Port}],
         #{env => #{dispatch => Dispatch}}
     ),
     secure_chat_sup:start_link().
